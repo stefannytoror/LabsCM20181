21cd15afcae91e745fcde18331c63cfab8978dad
stefannytoror <stefannyrami@gmail.com>
2018-03-12 09:18:30 -0500

    modulo lab2Activities

---
+++ lab2activities/src/main/java/co/edu/ude/compumovilgr02_20181/MainActivity.java
@@ -0,0 +1,13 @@
+package co.edu.ude.compumovilgr02_20181;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
